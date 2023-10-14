85ee7fa202837162c6129a4244f362df294d40e9
esciafardini <edward.ciafardini@aclaimant.com>
2023-10-14 00:15:36 -0400

    Add Re-Frame and Reagent to app
    This PR represents a working boilerplate version of the app

--- src/cljs/yogasync/core.cljs
+++ src/cljs/yogasync/core.cljs
@@ -1,6 +1,18 @@
-(ns yogasync.core)
+(ns yogasync.core
+  (:require
+   [reagent.dom :as rdom]
+   [re-frame.core :as re-frame]
+   [yogasync.events :as events]
+   [yogasync.views :as views]))
+
+(defn ^:dev/after-load mount-root []
+  (re-frame/clear-subscription-cache!)
+  (let [root-el (.getElementById js/document "app")]
+    (rdom/unmount-component-at-node root-el)
+    (rdom/render [views/main-panel] root-el)))
 
 (defn ^:export init []
-  (js/console.log "hey haha"))
+  (js/console.log "HAHAHA")
+  (re-frame/dispatch-sync [::events/initialize-db])
+  (mount-root))
 
-(js/console.log "HAHAHA")
---
+++ src/cljs/yogasync/views.cljs
@@ -0,0 +1,10 @@
+(ns yogasync.views
+  (:require
+   [re-frame.core :as re-frame]
+   [yogasync.subs :as subs]))
+
+(defn main-panel []
+  (let [name (re-frame/subscribe [::subs/name])]
+    [:div
+     [:h1
+      "Hello from " @name]]))
